075fb01e175c4f997465bc0f4dbbfc7b675f8405
user2230 <furkanefeceylan2008@hotmail.com>
2025-05-15 12:57:53 +0200

    mid

---
+++ 02 data/sql/02 eigendata.sql
@@ -0,0 +1,8 @@
+SELECT logo, songs, volume, id, artist FROM applicaties;
+
+
+SELECT id, songs, logo, volume, artist FROM applicaties;
+
+
+SELECT artist, volume, songs, logo, id FROM applicaties;
+
---
+++ 02 data/sql/02 huisdierselect.sql
@@ -0,0 +1,5 @@
+SELECT * FROM huisdieren;
+
+SELECT naam,leeftijd,eigenaar FROM huisdieren;
+
+SELECT eigenaar,naam FROM huisdieren;
